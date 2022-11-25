686746527a1c5295828668ea472b9a6a94df1719
Prashant Mishra <103806839+pr-a-s-h-a-n-t@users.noreply.github.com>
2022-11-25 10:00:32 +0000

    Pending changes exported from your codespace

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Video Conferencing App</h1>
+      <p ata-ns-test="project-description">This is a project description</p>
+    </div>
   )
 }
 
